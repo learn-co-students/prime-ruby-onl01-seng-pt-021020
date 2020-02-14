dd3ff8c556414a84b560e0ac85333a3da71524ad
Steven Riggs <sfriggs21@gmail.com>
2020-02-14 03:02:28 -0700

    final solution

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  limit = (num / 2.0).ceil - 1
+  checked = []
+  
+  (2..limit).each do |check_num|
+    if !checked.reverse.include?(check_num)
+      if num % check_num != 0
+        checked += (check_num..limit).step(check_num).to_a
+      else
+        return false
+      end
+    end
+  end
+  return true
+end
