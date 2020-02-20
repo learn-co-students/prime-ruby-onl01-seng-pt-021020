aa482d8700e15fd93a75c655e6ebf11ec034a7b5
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-20 00:07:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+    i = 2
+    if integer.negative? || integer < 2
+      return false
+    elsif i < integer
+
+      range = (i..integer - 1).to_a
+    range.collect {|b| return false if integer % b == 0 }
+    end
+    true
+end
