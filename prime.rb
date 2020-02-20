0ef4cd67ab6b0827afccce25ea37acfa8ef09972
tyler jones <rteyl1690@gmail.com>
2020-02-20 01:15:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(i)
+  n = 2
+  if i < 2
+    return false
+  elsif i == 2
+    return true
+  end
+  while n < i do
+    if i % n == 0
+      return false
+    else
+    n += 1
+    end
+  end
+  return true
+end
