a822efb8f7fbeea21d15124eff910147ffb132fa
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 23:34:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2 
+  if number > 1
+    num_range = (start..number-1).to_a
+    num_range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
